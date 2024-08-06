42b5b7f9b682ab7cfac9008b45910366be5e7988
Saumya Patel <159504605+snp05016@users.noreply.github.com>
2024-08-06 12:24:16 +0400

    add stopwatch

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+// import Student from './Student.jsx'
+import List from './List.jsx'
+import UserGreeting from "./UserGreeting.jsx"
+import Button from "./Button.jsx"
+import ProfilePicure from "./ProfilePicrture.jsx"
+import MyComponent from './MyComponent.jsx'
+import Counter from './Counter.jsx'
+import ColorPicker from './ColorPicker.jsx'
+import Stopwatch from './stop.jsx'
+function App() {
+  return (
+    <>
+      <Stopwatch/>
+    </>
+  )
+}
+
+export default App
+
